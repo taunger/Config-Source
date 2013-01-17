a42c2c93ff07a2baec345c66b0719b8d9bd7f2a6
Tarek Unger <tu2@gmx.net>
2013-01-17 18:08:50 +0100

    0.03
    Signed-off-by: Tarek Unger <tu2@gmx.net>

--- lib/Config/Source.pm
+++ lib/Config/Source.pm
@@ -12,11 +12,11 @@ Config::Source - manage a configuration from multiple sources
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
